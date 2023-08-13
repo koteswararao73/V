b168183c820959111fd8795e5d35a77be97fd855
koteswaraRao Sairi <116053113+koteswararao73@users.noreply.github.com>
2023-08-13 20:26:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ provider "aws" {
 resource "aws_instance" "koteswa" {
     ami = "ami-08a52ddb321b32a8c"
     instance_type = "t2.micro"
-    key_name = "aws_key_pair.key.id"
     subnet_id = "subnet-036d83bfc74e674ac"
-    security_groups = [ "sg-0132e4b6e77570d07" ]
+    security_groups = ["sg-0132e4b6e77570d07"]
+    key_name = "aws_key_pair.key.id"
     tags = {
         Name = "gabbeiir"
     }
