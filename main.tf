d8ebadd0cec3dcda8fbca71a76ccabe70ecbe58a
koteswaraRao Sairi <116053113+koteswararao73@users.noreply.github.com>
2023-08-13 20:19:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "koteswa" {
     ami = "ami-08a52ddb321b32a8c"
-    instance_type = t2.micro
+    instance_type = "t2.micro"
     key_name = "aws_key_pair.key.id"
     vpc_id   = "vpc-0a0379e470d547514"
     subnet_id = "subnet-036d83bfc74e674ac"
