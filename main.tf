502c52b797f75cef22541802539a28dbfaae7511
koteswaraRao Sairi <116053113+koteswararao73@users.noreply.github.com>
2023-08-13 20:18:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "koteswa" {
   
 }
 resource "aws_key_pair" "key" {
-    name = "kote"
+    key_name = "kote"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJmtu4BSVYCZXE3ZuIxkaF7sol5P8CTBLu8m7bFUCKtwruShMTuHwnjOl0YRZy+SCtvireNebXhMKR/OOX7EdfMpYISu6+JoPFSDX3LbbX4ATJW53GaVarcVldlYquGYvnpMEPNC1acthoOs94lzsiL8unlJ+14xJE+n+LBW45/XPKUaslA0vs2rMKUrfyHZnguIIai31AbgplQ8zA9bCSxPmy6ZMEj/ifnfg/d2WQEf+bdG4Fop90viheuTxQW8OKOsen2UcPLrfsusfArkn4Ai8ex7MSnb5L0RYu9AWPiQ6GieIw8OSzvTaSqGKlZK9sa1C4B7Ojq9y/vz4YhIoCKLgpHCDSCdYlqXH9h7uT0Zxm4rd5shHIw5xpXfwIKbx8QJLqrUsOcL95szO/F+B1VaudN0EZC97XkHiF4AlPoXiw2nlWaROTRGDUL/U3GB+R+RYnrqQPolZANQQ02ccvVzqQhsscxU0BQ/LCfSeGwYD9+8cjN2ZTv3gwfu/cSX0= kotes@MSI"
   
 }
